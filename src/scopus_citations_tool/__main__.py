8ae3e352ba2efd87379975da48c8d55d6f723ee1
Anatoliy Lazovyj <Lazoviy3000@ya.ru>
2025-06-27 04:17:26 +0300

    refactor: Refactrored __main__ script

--- src/scopus_citations_tool/__main__.py
+++ src/scopus_citations_tool/__main__.py
@@ -1,89 +1,76 @@
-from operator import attrgetter
-from typing import List, Tuple
-from datetime import datetime
-import csv
+import datetime as dt
 import re
+import secrets
+import time
 
-from pydantic import ValidationError
-from selenium import webdriver
+import pydantic
 from loguru import logger
+from orcid_scraping.models import Orcid, OrcidResearcherWorkUrlSite
+from orcid_scraping.models import ScrapeResult as OrcidScrapeResult
+from orcid_scraping.tools.selenium import OrcidScrapeToolSelenium
+from orcid_scraping.utils import sanitize_orcid
+from pydantic import ValidationError
+from scopus_scraping.models import ScrapeResult as ScopusScrapeResult
+from scopus_scraping.tools import ScopusScrapeToolSelenium
+from selenium import webdriver
 from tqdm import tqdm
 
-from scrappers.orcid import OrcidId, OrcidWork
-from scrappers.scopus import ScopusRecords
-from scrappers import Orcid, Scopus
-
-from cli import cli_settings
-import logger_config
+from scopus_citations_tool.cli import cli_settings
+from scopus_citations_tool.exporters import CsvExporter
+from scopus_citations_tool.logger import logger_config
 
 
 def scrap_works_records(
-    driver: webdriver.Firefox, orcid: OrcidId
-) -> List[Tuple[OrcidWork, ScopusRecords]]:
-    orcid_works = Orcid.get_works(driver, [orcid])[0]
-    scop_urls = list(map(attrgetter("scopus_url"), orcid_works))
-    scops_records = Scopus.get_records(driver, scop_urls)
-    return list(zip(orcid_works, scops_records, strict=True))
+    driver: webdriver.Firefox,
+    orcid: Orcid,
+) -> list[tuple[OrcidScrapeResult, ScopusScrapeResult]]:
+    orcid_tool = OrcidScrapeToolSelenium(driver=driver)
+    scopus_tool = ScopusScrapeToolSelenium(driver=driver)
+    works = []
+    logger.info("Scraping ORCID works")
+    orcid_scrape_result = orcid_tool.scrape_works(orcid=orcid)
+    orcid_works_iter = tqdm(
+        orcid_scrape_result.works,
+        desc="Scrapping Scopus citations",
+        unit="scopus page",
+        delay=0.5,
+    )
+    for orcid_work in orcid_works_iter:
+        if (
+            orcid_work.url is not None
+            and orcid_work.url.source == OrcidResearcherWorkUrlSite.SCOPUS
+        ):
+            time.sleep(secrets.randbelow(3))
+            citations = scopus_tool.scrape_citations(url=str(orcid_work.url.value))
+            works.append((orcid_work, citations))
+    return works
 
 
-def parse_to_csv(
-    works_records: List[Tuple[OrcidWork, ScopusRecords]],
-    out_filename: str,
-) -> None:
-    logger.info(f"Writing to csv file: {out_filename}")
-    with open(out_filename, "w", newline="", encoding="utf-8") as out:
-        csv_w = csv.writer(out)
-        csv_title = (
-            "Work title",
-            "Citations overall",
-            "Citations available",
-            "Citations",
-        )
-        csv_w.writerow(csv_title)
-        works_records_iter = tqdm(
-            works_records, desc="Writing to csv file", unit="row", delay=0.5
-        )
-        for work, records in works_records_iter:
-            first_row = (work.title, 0, 0, None)
-            if records is not None:
-                first_record = records.records[0].title if records.records else None
-                first_row = (
-                    work.title,
-                    records.count,
-                    len(records.records),
-                    first_record,
-                )
-            csv_w.writerow(first_row)
-            if records:
-                for i in range(1, len(records.records)):
-                    csv_w.writerow((None, None, None, records.records[i].title))
-    logger.success(f"Result saved to {out_filename}")
-
-
-def get_most_unique_filename(orcid_id: OrcidId) -> str:
-    dt_norm = str(datetime.now())
+@pydantic.validate_call(validate_return=True)
+def get_most_unique_filename(orcid: Orcid) -> str:
+    dt_norm = str(dt.datetime.now(dt.UTC))
     dt_norm = re.sub(r"\:|\.| ", "-", dt_norm)
-    return f"{orcid_id.id}_{dt_norm}"
+    return f"{orcid}_{dt_norm}"
 
 
 def _get_driver() -> webdriver.Firefox:
     driver_opts = webdriver.FirefoxOptions()
-    driver_opts.add_argument("--headless")
+    # driver_opts.add_argument("--headless")
     driver: webdriver.Firefox = webdriver.Firefox(driver_opts)
     return driver
 
 
-def main():
+def main() -> None:
     orcid_str = input("Enter ORCID (ID or URL): ")
     out_dir = cli_settings.out_dir
 
     driver = None
     try:
         driver = _get_driver()
-        orcid_id = OrcidId(id=orcid_str)
+        orcid_id = sanitize_orcid(orcid_str)
         filename = get_most_unique_filename(orcid_id)
         works_records = scrap_works_records(driver, orcid_id)
-        parse_to_csv(works_records, f"{out_dir}\{filename}.csv")
+        CsvExporter.export_to_file(works_records, rf"{out_dir}\{filename}.csv")
     except ValidationError as err:
         for error in err.errors():
             logger.error(error["ctx"]["error"])
@@ -91,11 +78,9 @@ def main():
         logger.exception(err)
         logger.info("Unexpected exceptions has occurred")
     finally:
-        logger.info(
-            f"Log saved to {cli_settings.log_dir}\{logger_config.LOG_FILENAME_DEFAULT}"
-        )
+        logger.info(rf"Log saved to {cli_settings.log_dir}\{logger_config.LOG_FILENAME_DEFAULT}")
         if driver is not None:
-            driver.close()
+            driver.quit()
 
 
 if __name__ == "__main__":
